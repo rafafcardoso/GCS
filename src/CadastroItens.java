82766183ef4c420f91f32ed5b0ef8002cbb8bda2
RafaelKreutz <rafaelmkreutz@gmail.com>
2024-10-07 18:55:46 -0300

    Cadastro dos Primeiros 10 Itens

---
+++ src/CadastroItens.java
@@ -0,0 +1,17 @@
+public class CadastroItens {
+    //Itens Fixos
+    public void itensFixos(){
+        //nome,categoria,descricao
+         Item espadadodivino = new Item("Espada do Divino","Espada","Dano Crítico");
+         Item manopladoburaconegro = new Item("Manopla do Buraco Negro", "Manopla", "Tanque");
+         Item decapitador = new Item("Decapitador", "Espada", "Dano de Ultimate");
+         Item forcadovendaval = new Item("Forca Do Vendaval", "Arco", "Dano por Segundo");
+         Item comecarne = new Item("Come Carne", "Adaga", "Dano e Cura");
+         Item sedentaporsange = new Item("Sedenta por Sangue","Espada","Dano e Roubo de Vida");
+         Item virtuderadiante = new Item("Virtude Radiante","Armadura","Tanque e Cura");
+         Item laminadamiragem = new Item("Lamina da Miragem", "Adaga","Dano e Velocidade de Ataque");
+         Item mascaragemea = new Item("Máscara Gêmea","Armadura","Dano, Vida, Velocidade de Ataque e Vida");
+         Item reverberacao = new Item("Reverberação","Armadura","Vida e Resistência");
+         Item eclipse = new Item("Eclipse","Adaga","Dano e Escudo");
+    }
+}
---
+++ src/Item.java
@@ -0,0 +1,28 @@
+public class Item {
+    private String nome;
+    private String categoria;
+    private String description;
+    public Item(String nome, String categoria, String description) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.description = description;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
