0d014974a1b0cd9cabd58d0572ab04d470e8c847
Guilherme <guilherme.fell002@edu.pucrs.br>
2024-10-10 12:06:37 -0300

    Adição do metodo ordenador em ordem alfabetica.

--- src/Inventario.java
+++ src/Inventario.java
@@ -9,9 +9,28 @@ public class Inventario {
     //metodo que adiciona itens a lista.
     public void addItem(Item item){
         Invent.add(item);
+        }
 
+        //metodo que ordena os itens da lista em ordem alfabetica usando ordenação.
+        public void OrdenarPorAlfabeto(){
+        for(int i=0; i<Invent.size()-1; i++){
+            for(int j=i+1; j<Invent.size(); j++){
+                if(Invent.get(i).getNome().compareTo(Invent.get(j).getNome()) > 0){
+                        Item aux = Invent.get(i);
+                        Invent.set(i, Invent.get(j));
+                        Invent.set(j, aux);
+                    }
+                }
+            }
+            System.out.println("Ordenando Itens...");
+        for(Item item : Invent){
+            System.out.println(item.getNome());
         }
 
         }
+
+        }
+
+
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        App app = new App();
-        app.executar();
-
-
+        System.out.println("Hello world!");
     }
 }
